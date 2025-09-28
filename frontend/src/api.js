5183ceeac849508acffcc9a30223a67c7ce79d29
Rahul Panjiyara <rahulpanjiyara@gmail.com>
2025-09-28 16:14:40 +0530

    added backend url on api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api", // API URL
+  baseURL: "https://xexit-5qgl.onrender.com/api", // API URL
 });
 
 API.interceptors.request.use((req) => {
